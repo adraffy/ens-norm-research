c4484053a29bb3f117ea4fbba6f8e60c0444a0fd
Andrew Raffensperger <raffy@me.com>
2022-06-29 04:13:01 -0700

    wrong contract

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ens-emoji
 
-Compress 3406 emoji sequences (11K codepoints) into 10KB state machine to supplement the following API:
+Compress 3406 emoji sequences (11K codepoints) into 12KB state machine to supplement the following API:
 
 ```solidity
 function read(uint24[] memory cps, uint256 pos) view returns (uint256 len) {
@@ -15,4 +15,4 @@ function read(uint24[] memory cps, uint256 pos) view returns (uint256 len) {
 1. Acquire `ens-normalize-hr.json` from [@adraffy/ens-normalize.js](https://github.com/adraffy/ens-normalize.js)
 1. Run `compress.js` to build rules
 1. Run `check.js` to validate rules
-1. Run `payload.js` to create hex payload
+1. Run `payload.js` to create compressed payload 
--- contracts/EmojiParser.sol
+++ contracts/EmojiParser.sol
@@ -5,21 +5,37 @@ contract EmojiParser {
  
 	mapping (uint256 => uint256) emoji;
 
-	function upload(bytes memory v) public {
+	function upload(bytes calldata data) public {
 		uint256 i;
 		uint256 e;
-		uint256 x;
 		assembly {
-			i := v
-			e := sub(add(v, mload(v)), 1)
+			i := data.offset
+			e := add(i, data.length)
 		}
 		while (i < e) {
+			uint256 k;
+			uint256 v;
 			assembly {
-				i := add(i, 6) 
-				x := mload(i) 
+				v := calldataload(i)
+				i := add(i, 4)
+				k := and(calldataload(i), 0xFFFFFFFF)
+				i := add(i, 32)
 			}
-			emoji[(x >> 16) & 0xFFFFFFFF] = x & 0xFFFF;
+			emoji[k] = v;
 		}
+	}
+
+	function get_debug(uint256 state0, uint256 cp) public view returns (uint256 state1, uint256 eat, bool fe0f, bool save, bool check) {
+		uint256 value = get(state0, cp);
+		state1 = value & 0xFF;
+		eat    = (value & 0x1E00) >> 9;
+		fe0f   = (value & 0x8000) != 0;
+		save   = (value & 0x4000) != 0;
+		check  = (value & 0x2000) != 0;
+	}
+
+	function get(uint256 state0, uint256 cp) public view returns (uint256 value) {
+		value = (emoji[((state0 & 0xFF) << 20) | (cp >> 4)] >> ((cp & 0xF) << 4)) & 0xFFFF;
 	}
 
 	function read(uint24[] memory cps, uint256 pos) public view returns (uint256 len) {
@@ -28,7 +44,7 @@ contract EmojiParser {
 		uint256 fe0f;
 		uint256 extra;
 		uint256 saved;
-		while (pos < cps.length) {
+		unchecked { while (pos < cps.length) {
 			uint256 cp = cps[pos++]; 
 			if (cp == 0xFE0F) {
 				if (fe0f == 0) break;
@@ -39,9 +55,9 @@ contract EmojiParser {
 					extra++;
 				}
 			} else {
-				state = emoji[((state & 0xFF) << 24) | cp]; 
+				state = get(state, cp);
 				if (state == 0) break;
-				eat = (state & 0x1C00) >> 8;
+				eat = (state & 0x1E00) >> 9;
 				len += eat;
 				if (extra > 0 && eat > 0) {
 					len += extra;
@@ -54,6 +70,76 @@ contract EmojiParser {
 					saved = cp;
 				}
 			}
+		} }
+	}
+
+	function filter(string memory s) public view returns (uint24[] memory cps) {
+		cps = decodeUTF8(bytes(s));
+		uint256 pos;
+		uint256 out;
+		while (pos < cps.length) {
+			uint24 cp = cps[pos];
+			if (cp == 0xFE0F) { // ignored
+				pos++;
+				continue;
+			}
+			uint256 len = read(cps, pos);
+			if (len > 0) { // emoji
+				cps[out++] = 0xFFFFFF;
+				pos += len;
+			} else { // non-emoji
+				cps[out++] = cp;
+				pos++;
+			}
+		}
+		assembly { 
+			mstore(cps, out) 
+		}
+	}
+
+	function decode(string memory s) public pure returns (uint24[] memory cps) {
+		cps = decodeUTF8(bytes(s));
+	}
+
+	error InvalidUTF8();
+
+	function decodeUTF8(bytes memory v) private pure returns (uint24[] memory cps) {
+		uint256 n = v.length;
+		cps = new uint24[](n);
+		uint256 i;
+		uint256 j;
+		unchecked { while (i < n) {
+			uint256 cp = uint8(v[i++]);
+			if ((cp & 0x80) == 0) { // [1] 0xxxxxxx
+				//
+			} else if ((cp & 0xE0) == 0xC0) { // [2] 110xxxxx (5)
+				if (i >= n) revert InvalidUTF8();
+				uint256 a = uint8(v[i++]);
+				if ((a & 0xC0) != 0x80) revert InvalidUTF8();
+				cp = ((cp & 0x1F) << 6) | a;
+				if (cp < 0x80) revert InvalidUTF8();
+			} else if ((cp & 0xF0) == 0xE0) { // [3] 1110xxxx (4)
+				if (i + 2 > n) revert InvalidUTF8();
+				uint256 a = uint8(v[i++]);
+				uint256 b = uint8(v[i++]);
+				if (((a | b) & 0xC0) != 0x80) revert InvalidUTF8();
+				cp = ((cp & 0xF) << 12) | ((a & 0x3F) << 6) | (b & 0x3F);
+				if (cp < 0x0800) revert InvalidUTF8();
+			} else if ((cp & 0xF8) == 0xF0) { // [4] 11110xxx (3)
+				if (i + 3 > n) revert InvalidUTF8();
+				uint256 a = uint8(v[i++]);
+				uint256 b = uint8(v[i++]);
+				uint256 c = uint8(v[i++]);
+				if (((a | b | c) & 0xC0) != 0x80) revert InvalidUTF8();
+				cp = ((cp & 0x7) << 18) | ((a & 0x3F) << 12) | ((b & 0x3F) << 6) | (c & 0x3F);
+				if (cp < 0x10000 || cp > 0x10FFFF) revert InvalidUTF8();
+			} else {
+				revert InvalidUTF8();
+			}
+			cps[j++] = uint24(cp);
+		} }
+		assembly { 
+			mstore(cps, j) 
 		}
 	}
 
